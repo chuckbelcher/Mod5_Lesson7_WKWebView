e9dce1b5da2a508438f81cebfd506233d3623305
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-29 20:25:41 -0400

    Initial Commit

---
+++ Mod5_Lesson7_WKWebView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mod5_Lesson7_WKWebView
+//
+//  Created by Chuck Belcher on 7/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mod5_Lesson7_WKWebView/Mod5_Lesson7_WKWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod5_Lesson7_WKWebViewApp.swift
+//  Mod5_Lesson7_WKWebView
+//
+//  Created by Chuck Belcher on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Mod5_Lesson7_WKWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
